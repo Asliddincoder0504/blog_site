a72493b2186400ef3131941c74a9318ca57b8c60
Asliddin <asliddincoder0504@gmail.com>
2025-12-17 09:22:19 +0500

    Initial commit

--- blogapp/views.py
+++ blogapp/views.py
@@ -8,12 +8,30 @@ from django.views.generic import ListView
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 4
     template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status="published", publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments=post.comments.filter(active=True)
+    print(comments)
+    new_comment=None
 
-
+#     if request.method=='POST':
+#         comment_form=CommentForm(data=request.POST)
+#         if comment_form.is_valid():
+#             new_comment=comment_form.save(commit=False)
+#             new_comment.post=post
+#             new_comment.save()
+#     else:
+#         comment_form=CommentFrom()
+#     return render(request,
+#                   'blog/post/detail.html',
+#                   {'post':post,
+#                    'comments':comments,
+#                    'new_comment':new_comment,
+#                    'comment_form':comment_form
+#                    })
+#
+# def post_share(request,post_id):
